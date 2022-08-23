acba71718ec85d644b1427e30c7622dfe7d41875
Sina Kiani <kianisina73@yahoo.com>
2022-08-23 13:50:29 +0200

    A new Class added and the calculation of the American Ten-Pin Bowling occur there.

--- src/main/java/kata/bowling/Game.java
+++ src/main/java/kata/bowling/Game.java
@@ -1,40 +1,19 @@
 package kata.bowling;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game {
-    private List<Frame> frames;
-
+    private AmericanTenPinsRules americanTenPinsRules;
     public Game() {
-        frames = new ArrayList<>();
+        americanTenPinsRules = new AmericanTenPinsRules();
     }
 
-    public void addFrame(int pinsDownInFirstRoll, int pinsDownInSecondRoll) {
-        frames.add(new Frame(pinsDownInFirstRoll, pinsDownInSecondRoll));
+    public void addFrame(Frame frame) {
+        americanTenPinsRules.addFrame(frame);
 
     }
 
     public int getScore() {
-        int score = 0;
-        for (int currentFrame = 0; currentFrame < frames.size(); currentFrame++) {
-            if (isStrike(currentFrame)) {
-                score += frames.get(currentFrame).getScore() + frames.get(currentFrame + 1).getScore();
-
-            } else if (isSpare(currentFrame)) {
-                score += frames.get(currentFrame).getScore() + frames.get(currentFrame + 1).getPinsDownInFirstRoll();
-            } else {
-                score += frames.get(currentFrame).getScore();
-            }
-        }
-        return score;
+        return americanTenPinsRules.calScore();
     }
 
-    private boolean isStrike(int currentFrame) {
-        return frames.get(currentFrame).getPinsDownInFirstRoll() == 10;
-    }
 
-    private boolean isSpare(int currentFrame) {
-        return frames.get(currentFrame).getScore() == 10;
-    }
 }
--- src/test/java/kata/bowling/GameTest.java
+++ src/test/java/kata/bowling/GameTest.java
@@ -15,51 +15,51 @@ class GameTest {
 
     @Test
     public void canScoreZero() {
-        game.addFrame(0, 0);
+        game.addFrame(new Frame(0, 0));
         assertEquals(0, game.getScore());
     }
 
     @Test
     public void canScoreOneAndTwo() {
-        game.addFrame(1, 2);
+        game.addFrame(new Frame(1, 2));
         assertEquals(3, game.getScore());
     }
 
     @Test
     public void canScoreMultipleNormalFrames() {
-        game.addFrame(2, 3);
-        game.addFrame(6, 2);
-        game.addFrame(5, 1);
-        game.addFrame(4, 3);
-        game.addFrame(6, 3);
-        game.addFrame(2, 4);
-        game.addFrame(3, 6);
-        game.addFrame(7, 2);
-        game.addFrame(4, 3);
-        game.addFrame(4, 3);
+        game.addFrame(new Frame(2, 3));
+        game.addFrame(new Frame(6, 2));
+        game.addFrame(new Frame(5, 1));
+        game.addFrame(new Frame(4, 3));
+        game.addFrame(new Frame(6, 3));
+        game.addFrame(new Frame(2, 4));
+        game.addFrame(new Frame(3, 6));
+        game.addFrame(new Frame(7, 2));
+        game.addFrame(new Frame(4, 3));
+        game.addFrame(new Frame(4, 3));
         assertEquals(73, game.getScore());
     }
 
     @Test
     public void canScoreSpare() {
-        game.addFrame(5, 5);
-        game.addFrame(6, 0);
-        game.addFrame(6, 4);
-        game.addFrame(7, 3);
-        game.addFrame(5, 3);
+        game.addFrame(new Frame(5, 5));
+        game.addFrame(new Frame(6, 0));
+        game.addFrame(new Frame(6, 4));
+        game.addFrame(new Frame(7, 3));
+        game.addFrame(new Frame(5, 3));
         for (int i = 0; i < 5; i++) {
-            game.addFrame(0, 0);
+            game.addFrame(new Frame(0, 0));
         }
         assertEquals(62, game.getScore());
     }
 
     @Test
     public void canScoreStrike() {
-        game.addFrame(10, 0);
-        game.addFrame(3, 7);
-        game.addFrame(3, 3);
+        game.addFrame(new Frame(10, 0));
+        game.addFrame(new Frame(3, 7));
+        game.addFrame(new Frame(3, 3));
         for (int i = 0; i < 7; i++) {
-            game.addFrame(0, 0);
+            game.addFrame(new Frame(0, 0));
         }
         assertEquals(39, game.getScore());
     }
